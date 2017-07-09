abf664d14e0e5fa53760ee6da576bda3c27b750f
Lucas Dohmen <lucas@dohmen.io>
2017-07-09 11:41:55 +0200

    Debouncing the notification

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ let chokidar = require("chokidar");
 let EventEmitter = require("events");
 let path = require("path");
 
-module.exports = rootDir => {
+module.exports = (rootDir, delay = 10) => {
 	let watcher = chokidar.watch(`${rootDir}/**`, {});
 	let emitter = new EventEmitter();
 
 	// NB: potentially invoked multiple times for a single change
-	let notify = filepath => {
+	let notify = debounce(delay, filepath => {
 		filepath = path.resolve(rootDir, filepath);
 		emitter.emit("edit", filepath);
-	};
+	});
 
 	watcher.on("ready", _ => {
 		watcher.on("add", notify).
@@ -20,4 +20,20 @@ module.exports = rootDir => {
 			on("unlink", notify);
 	});
 	return emitter;
+};
+
+// adapted from uitil <https://github.com/FND/uitil>
+function debounce(delay, fn) {
+	let timer;
+	return function() {
+		let args = arguments;
+		if(timer) {
+			clearTimeout(timer);
+			timer = null;
+		}
+		timer = setTimeout(_ => {
+			fn.apply(null, args);
+			timer = null;
+		}, delay);
+	};
 };
